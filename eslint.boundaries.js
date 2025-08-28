af52d5d21dfc1394c0d008e6e07435f61aedf165
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-08-28 09:13:30 +0300

    настроил eslint и prettier

---
+++ eslint.boundaries.js
@@ -0,0 +1,80 @@
+import boundaries from 'eslint-plugin-boundaries';
+
+export const eslintBoundariesConfig = {
+  plugins: {
+    boundaries
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    },
+
+    'boundaries/elements': [
+      {
+        type: 'app',
+        pattern: './src/app'
+      },
+      {
+        type: 'pages',
+        pattern: './src/pages/*'
+      },
+      {
+        type: 'features',
+        pattern: './src/features/*'
+      },
+      {
+        type: 'shared',
+        pattern: './src/shared/*'
+      }
+    ]
+  },
+  rules: {
+    'boundaries/element-types': [
+      2,
+      {
+        default: 'allow',
+        rules: [
+          {
+            from: 'shared',
+            disallow: ['app', 'pages', 'features'],
+            message:
+              'Модуль нижележащего слоя (${file.type}) не может импортировать модуль вышележащего слоя (${dependency.type})'
+          },
+          {
+            from: 'features',
+            disallow: ['app', 'pages'],
+            message:
+              'Модуль нижележащего слоя (${file.type}) не может импортировать модуль вышележащего слоя (${dependency.type})'
+          },
+          {
+            from: 'pages',
+            disallow: ['app'],
+            message:
+              'Модуль нижележащего слоя (${file.type}) не может импортировать модуль вышележащего слоя (${dependency.type})'
+          }
+        ]
+      }
+    ],
+    'boundaries/entry-point': [
+      2,
+      {
+        default: 'disallow',
+        message:
+          'Модуль (${file.type}) должен импортироваться через public API. Прямой импорт из ${dependency.source} запрещен',
+
+        rules: [
+          {
+            target: ['shared', 'app'],
+            allow: '**'
+          },
+          {
+            target: ['pages', 'features'],
+            allow: ['index.(ts|tsx)']
+          }
+        ]
+      }
+    ]
+  }
+};
